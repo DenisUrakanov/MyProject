b5b0e3a26fde319866da56d38819f04d51ad0697
Denis <papa149@mail.ru>
2018-07-26 13:13:58 +0500

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        City city = new City("London",27319,"LondonHymn");
+        System.out.println(city);
+    }
+}
